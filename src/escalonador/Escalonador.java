18177d786d407a553237fbd3b1d35ff3edee8916
Cleyton A Silva <cleyton.eu123@gmail.com>
2020-09-23 18:55:02 -0300

    Exlcuing some meme

--- src/escalonador/Escalonador.java
+++ src/escalonador/Escalonador.java
@@ -5,20 +5,7 @@ public class Escalonador {
     public static void main(String[] args) {
         System.out.println("Salve"); 
         Scanner scan = new Scanner(System.in);
-        Object zap = scan.next();
-        System.out.println((int)zap);
         
-        switch((int)zap){
-            case 1:
-                System.out.println("1");
-            break;
-            case 2:
-                System.out.println("2");
-            break;
-            default:
-                System.out.println("Deu cocô");
-            break;
-        }
     }
     
 }
